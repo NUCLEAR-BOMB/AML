82b1640b069910432af32b69bd9911c8a3d257ff
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2022-11-27 18:24:14 +0500

    Add AML_ASSUME and AML_UNREACHABLE

--- include/AML/_AMLCore.hpp
+++ include/AML/_AMLCore.hpp
@@ -3,3 +3,20 @@
 #define AML_NAMESPACE namespace aml {
 #define AML_NAMESPACE_END }
 
+#if defined(_MSC_VER)
+	#define AML_MSVC 1
+#else
+	#define AML_MSVC 0
+#endif
+
+#if AML_MSVC
+	#define AML_ASSUME(expression) __assume(expression)
+#else
+	#define AML_ASSUME(expression)
+#endif
+
+#if AML_MSVC
+	#define AML_UNREACHABLE __assume(0)
+#else
+	#define AML_UNREACHABLE __builtin_unreachable()
+#endif
