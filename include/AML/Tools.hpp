56685072514712e813bbda8d757f68776d78964c
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2022-11-30 20:19:48 +0500

    Add aml::*_from_bytes<size_t>, aml::*_from_bits<size_t> and aml::selectable_convert<Out, In>(In&&)

--- include/AML/Tools.hpp
+++ include/AML/Tools.hpp
@@ -6,6 +6,18 @@
 
 AML_NAMESPACE
 
+using selectable_unused_t = void;
+
+template<class Out, class In> [[nodiscard]] constexpr
+auto selectable_convert(In&& val) noexcept
+{
+	if constexpr (std::is_same_v<Out, selectable_unused_t>) {
+		return std::forward<In>(val);
+	} else {
+		return static_cast<Out>(val);
+	}
+}
+
 namespace detail {
 	template<class, class = std::size_t>
 	struct is_complete_impl : public std::false_type {};
@@ -82,6 +94,52 @@ void static_for(Function&& fun) noexcept
 {
 	detail::static_for_impl<static_cast<decltype(To)>(0), To>(std::forward<Function>(fun));
 }
+
+namespace detail
+{
+	template<std::size_t Bytes>
+	struct signed_from_bytes_impl
+	{
+		using type = 
+			std::conditional_t<Bytes <= 1, std::int8_t,
+			std::conditional_t<Bytes <= 2, std::int16_t,
+			std::conditional_t<Bytes <= 4, std::int32_t,
+			std::conditional_t<Bytes <= 8, std::int64_t,
+			void
+		>>>>;
+	};
+
+	template<std::size_t Bytes>
+	struct floating_point_from_bytes_impl
+	{
+		using type =
+			std::conditional_t<Bytes <= sizeof(float), float,
+			std::conditional_t<Bytes <= sizeof(double), double,
+			std::conditional_t<(sizeof(long double) > sizeof(double)), long double,
+			void
+		>>>;
+	};
+}
+
+
+template<std::size_t Bytes>
+using signed_from_bytes = typename detail::signed_from_bytes_impl<Bytes>::type;
+
+template<std::size_t Bytes>
+using unsigned_from_bytes = std::make_unsigned_t<signed_from_bytes<Bytes>>;
+
+template<std::size_t Bits>
+using signed_from_bits = signed_from_bytes<(Bits + (CHAR_BIT - 1)) / CHAR_BIT>;
+
+template<std::size_t Bits>
+using unsigned_from_bits = std::make_unsigned_t<signed_from_bits<Bits>>;
+
+
+template<std::size_t Bytes>
+using floating_point_from_bytes = typename detail::floating_point_from_bytes_impl<Bytes>::type;
+
+template<std::size_t Bits>
+using floating_point_from_bits = floating_point_from_bytes<(Bits + (CHAR_BIT - 1)) / CHAR_BIT>;
 
 
 AML_NAMESPACE_END
--- unittests/Tools.cpp
+++ unittests/Tools.cpp
@@ -53,6 +53,48 @@ TEST(static_for_test, force_max)
 	ASSERT_EQ(a, 123);
 }
 
+static constexpr
+void test_of_bytes() {
+	using namespace std;
+	static_assert(is_same_v<std::make_unsigned_t<signed_from_bytes<0>>, unsigned_from_bytes<0>>);
+
+	static_assert(is_same_v<signed_from_bytes<0>, int8_t>);
+	static_assert(is_same_v<signed_from_bytes<1>, int8_t>);
+	static_assert(is_same_v<signed_from_bytes<2>, int16_t>);
+	static_assert(is_same_v<signed_from_bytes<3>, int32_t>);
+	static_assert(is_same_v<signed_from_bytes<4>, int32_t>);
+	static_assert(is_same_v<signed_from_bytes<5>, int64_t>);
+	static_assert(is_same_v<signed_from_bytes<6>, int64_t>);
+	static_assert(is_same_v<signed_from_bytes<7>, int64_t>);
+	static_assert(is_same_v<signed_from_bytes<8>, int64_t>);
+
+	static_assert(0 > sizeof(float) || is_same_v<floating_point_from_bytes<0>, float>);
+	static_assert(1 > sizeof(float) || is_same_v<floating_point_from_bytes<1>, float>);
+	static_assert(2 > sizeof(float) || is_same_v<floating_point_from_bytes<2>, float>);
+	static_assert(3 > sizeof(float) || is_same_v<floating_point_from_bytes<3>, float>);
+	static_assert(4 > sizeof(float) || is_same_v<floating_point_from_bytes<4>, float>);
+}
+
+static constexpr
+void test_of_bits() {
+	using namespace std;
+	static_assert(is_same_v<std::make_unsigned_t<signed_from_bits<0>>, unsigned_from_bits<0>>);
+
+	static_assert(is_same_v<signed_from_bits<0>, int8_t>);
+	static_assert(is_same_v<signed_from_bits<8>, int8_t>);
+	static_assert(is_same_v<signed_from_bits<9>, int16_t>);
+	static_assert(is_same_v<signed_from_bits<16>, int16_t>);
+	static_assert(is_same_v<signed_from_bits<17>, int32_t>);
+	static_assert(is_same_v<signed_from_bits<32>, int32_t>);
+	static_assert(is_same_v<signed_from_bits<33>, int64_t>);
+	static_assert(is_same_v<signed_from_bits<64>, int64_t>);
+
+	static_assert(0 > sizeof(float) || is_same_v<floating_point_from_bits<0>, float>);
+	static_assert(1 > sizeof(float) || is_same_v<floating_point_from_bits<15>, float>);
+	static_assert(2 > sizeof(float) || is_same_v<floating_point_from_bits<17>, float>);
+	static_assert(3 > sizeof(float) || is_same_v<floating_point_from_bits<31>, float>);
+	static_assert(4 > sizeof(float) || is_same_v<floating_point_from_bits<32>, float>);
+}
 
 
 }
